c09327c676b44151a2d9359628f0ec38ab4aeef0
zYx <62143620+yunx-z@users.noreply.github.com>
2023-11-27 17:55:52 -0500

    Update basic_operations.py

--- tensorflow_v1/examples/1_Introduction/basic_operations.py
+++ tensorflow_v1/examples/1_Introduction/basic_operations.py
@@ -9,32 +9,21 @@ from __future__ import print_function
 
 import tensorflow as tf
 
-# Basic constant operations
-# The value returned by the constructor represents the output
-# of the Constant op.
 a = tf.constant(2)
 b = tf.constant(3)
 
-# Launch the default graph.
 with tf.Session() as sess:
     print("a=2, b=3")
     print("Addition with constants: %i" % sess.run(a+b))
     print("Multiplication with constants: %i" % sess.run(a*b))
 
-# Basic Operations with variable as graph input
-# The value returned by the constructor represents the output
-# of the Variable op. (define as input when running session)
-# tf Graph input
 a = tf.placeholder(tf.int16)
 b = tf.placeholder(tf.int16)
 
-# Define some operations
 add = tf.add(a, b)
 mul = tf.multiply(a, b)
 
-# Launch the default graph.
 with tf.Session() as sess:
-    # Run every operation with variable input
     print("Addition with variables: %i" % sess.run(add, feed_dict={a: 2, b: 3}))
     print("Multiplication with variables: %i" % sess.run(mul, feed_dict={a: 2, b: 3}))
 
